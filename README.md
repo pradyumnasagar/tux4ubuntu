64292efaa8d8c6f2f04c5476e980b04b62e8a9d8
tuxedojoe <nottoday@gmail.com>
2016-11-07 21:51:47 +0100

    Added a first version of an installer

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # tux4ubuntu-plymouth-theme
-Let's bring Tux to Ubuntu
+Let's bring Tux to Ubuntu!
+
+Ubuntu is nice but it needs a little Tux. Here he is, the man himself in the bootscreen, xsplash or as a Plymouth theme (whatever you would call it.
+
+Written and designed by Tuxedo Joe <http://github.com/tuxedojoe> for The Tux4Ubuntu Initiative <http://tux4ubuntu.blogspot.com>. Based on the Plymouth example provided with the "script plugin" written by Charlie Brej <cbrej@cs.man.ac.uk> Original Tux illustration by Larry Ewing <http://isc.tamu.edu/~lewing/linux/>, redrawn in Inkscape by Garrett LeSage <https://github.com/garrett/Tux>.
+
+Either run the install.sh script (write "./install.sh" when in this folder using the terminal), or just type the following commands:
+
+      Copy the theme (earlier Ubuntu versions had the themes in /lib/plymouth/themes/)
+          1) sudo cp -r tux4ubuntu/ /usr/share/plymouth/themes/
+      Add the theme to Plymouth (remember to change the folder adresses if needed)
+          2) sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/tux4ubuntu/tux4ubuntu.plymouth 100
+      Change the default theme, follow the instructions
+          3) sudo update-alternatives --config default.plymouth
+      Update initramfs"
+          4) sudo update-initramfs -update
+
+Let us know what you think!
---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+printf "\033c"
+echo "#######################################################################################"
+echo "# TUX 4 UBUNTU POLYMOUTH THEME                                                        #" 
+echo "#######################################################################################"
+echo "Hi and thanks for bringing Tux to Ubuntu!"
+echo ""
+echo "Are you running Ubuntu 16.04 LTS?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "Do you understand that changing bootlogo is not without risk? Internet can help, but nothing is 100% safe.";
+              select yn in "Yes" "No"; do
+                  case $yn in
+                    Yes ) echo "Ok, here we go! Remember to choose tux4ubuntu in the Plymouth menu next (if you want Tux that is ;)";
+                          read -n1 -r -p "Press any key to continue..." key
+                          echo ""
+                          echo "But first, Tux need your sudo allowance to add your theme to system folders:"
+                          sudo cp -r tux4ubuntu/ /usr/share/plymouth/themes/;
+                          sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/tux4ubuntu/tux4ubuntu.plymouth 100;
+                          sudo update-alternatives --config default.plymouth;
+                          sudo update-initramfs -u;
+                          break;;
+                    No ) exit;;
+                    esac
+                done
+              break;;
+        No )  echo "Can't guarantee anything but just typing these commands and following instructions should do it:"
+              echo ""
+              echo "      Copy the theme (earlier Ubuntu versions had the themes in /lib/plymouth/themes/)"
+              echo "          1) sudo cp -r tux4ubuntu/ /usr/share/plymouth/themes/"
+              echo "      Add the theme to Plymouth (remember to change the folder adresses if needed)"
+              echo "          2) sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/tux4ubuntu/tux4ubuntu.plymouth 100"
+              echo "      Change the default theme, follow the instructions"
+              echo "          3) sudo update-alternatives --config default.plymouth"
+              echo "      Update initramfs"
+              echo "          4) sudo update-initramfs -u"
+              echo ""
+              echo "Good luck!"
+              exit;;
+    esac
+done
