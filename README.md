25bc7a9520d078f88e35757888db77688ecc403e
tuxedojoe <nottoday@gmail.com>
2016-11-13 10:57:28 +0100

    Updated to use "tux" instead of "tux4ubuntu" as filename

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# tux4ubuntu-plymouth-theme
+# tux-plymouth-theme
 Let's bring Tux to Ubuntu!
 
 Ubuntu is nice but it needs a little Tux. Here he is, the man himself in the bootscreen, xsplash or as a Plymouth theme (whatever you would call it.
@@ -8,9 +8,9 @@ Written and designed by Tuxedo Joe <http://github.com/tuxedojoe> for The Tux4Ubu
 Either run the install.sh script (write "./install.sh" when in this folder using the terminal), or just type the following commands:
 
       Copy the theme (earlier Ubuntu versions had the themes in /lib/plymouth/themes/)
-          1) sudo cp -r tux4ubuntu/ /usr/share/plymouth/themes/
+          1) sudo cp -r tux/ /usr/share/plymouth/themes/
       Add the theme to Plymouth (remember to change the folder adresses if needed)
-          2) sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/tux4ubuntu/tux4ubuntu.plymouth 100
+          2) sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/tux/tux.plymouth 100
       Change the default theme, follow the instructions
           3) sudo update-alternatives --config default.plymouth
       Update initramfs"
--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -10,18 +10,19 @@ echo ""
 echo "Are you running Ubuntu 16.04 LTS?"
 
 # TODO: Add to search on /lib/plymouth/themes. On other systems it is /usr/share/plymouth
+#       More info on other OSes: http://brej.org/blog/?p=158
 
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) echo "Do you understand that changing bootlogo is not without risk? Internet can help, but nothing is 100% safe.";
               select yn in "Yes" "No"; do
                   case $yn in
-                    Yes ) echo "Ok, here we go! Remember to choose tux4ubuntu in the Plymouth menu next (if you want Tux that is ;)";
+                    Yes ) echo "Ok, here we go! Remember to choose tux in the Plymouth menu next (if you want Tux that is ;)";
                           read -n1 -r -p "Press any key to continue..." key
                           echo ""
                           echo "But first, Tux need your sudo allowance to add your theme to system folders:"
-                          sudo cp -r tux4ubuntu-plymouth-theme/ /usr/share/plymouth/themes/;
-                          sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/tux4ubuntu-plymouth-theme/tux4ubuntu.plymouth 100;
+                          sudo cp -r tux-plymouth-theme/ /usr/share/plymouth/themes/;
+                          sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/tux-plymouth-theme/tux.plymouth 100;
                           sudo update-alternatives --config default.plymouth;
                           sudo update-initramfs -u;
                           break;;
@@ -32,9 +33,9 @@ select yn in "Yes" "No"; do
         No )  echo "Can't guarantee anything but just typing these commands and following instructions should do it:"
               echo ""
               echo "      Copy the theme (earlier Ubuntu versions had the themes in /lib/plymouth/themes/)"
-              echo "          1) sudo cp -r tux4ubuntu/ /usr/share/plymouth/themes/"
+              echo "          1) sudo cp -r tux/ /usr/share/plymouth/themes/"
               echo "      Add the theme to Plymouth (remember to change the folder adresses if needed)"
-              echo "          2) sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/tux4ubuntu/tux4ubuntu.plymouth 100"
+              echo "          2) sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/tux/tux.plymouth 100"
               echo "      Change the default theme, follow the instructions"
               echo "          3) sudo update-alternatives --config default.plymouth"
               echo "      Update initramfs"
